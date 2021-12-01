803179fbabb30dc56b9f9ca29feda2dc848ad17b
Ernesto Garcia <egarcia2020@yahoo.com>
2021-12-01 00:04:39 -0800

    added latest code

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,100 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+   
+    const postData = await Post.findAll()
+ const posts = postData.map((p) => p.get({plain:true}))
+
+    res.render('homepage', {posts});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+   
+
+    res.render('login');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', async (req, res) => {
+  try {
+   
+
+    res.render('createpost');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+   
+router.get('/newPost', async (req, res) => {
+  try {
+   
+
+    res.render('createpost');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+   
+
+// router.get('/project/:id', async (req, res) => {
+//   try {
+//     const projectData = await Project.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
+
+//     const project = projectData.get({ plain: true });
+
+//     res.render('project', {
+//       ...project,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// // Use withAuth middleware to prevent access to route
+// router.get('/profile', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Project }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render('profile', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/login', (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect('/dashboard');
+//     return;
+//   }
+
+//   res.render('login');
+// });
+
+module.exports = router;
